ada1758a62b2f657cdbc1af016c73f5351b5b166
mtauha <111292199+mtauha@users.noreply.github.com>
2023-10-24 03:17:55 +0500

    Feat: New Simple API's defined

--- env/src/main.py
+++ env/src/main.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get(url="/")
-async def root():
-    return {"message":"Hello World!"}
+class Post(BaseModel):
+    title: str
+    content: str
 
-@app.get(url="/posts")
+
+@app.get("/")
+async def root():
+    return {"message":"Welcome to my API"}
+
+@app.get("/posts")
 async def get_post():
     return {"data":"This is your posts"}
+
+@app.post("/creatingposts")
+async def create_posts(post: Post):
+    print(post)
+    return {"data":"new_post"}
 
 #* NOTES:
 
