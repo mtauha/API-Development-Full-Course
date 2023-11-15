e674ec86fbaa6b41f137cd8f38661d8951379435
mtauha <111292199+mtauha@users.noreply.github.com>
2023-11-16 00:51:44 +0500

    Add status code to POST request for creating posts

--- env/src/main.py
+++ env/src/main.py
@@ -32,7 +32,7 @@ async def root():
 async def get_posts():
     return {"data":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000000)
