93f957b03dbb574da5587defcb779c406d742e11
mtauha <111292199+mtauha@users.noreply.github.com>
2023-11-16 00:50:09 +0500

    Refactor get_post function to remove unnecessary parameter

--- env/src/main.py
+++ env/src/main.py
@@ -46,7 +46,7 @@ def get_latest_post():
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
+def get_post(id: int):
     post = find_id(id=id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {id} not found")
