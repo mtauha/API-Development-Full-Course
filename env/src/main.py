a483d12cdf09df002967e2d4d13eb44589bd152e
mtauha <111292199+mtauha@users.noreply.github.com>
2023-11-16 00:48:41 +0500

    Add endpoints to get latest post and get post by id

--- env/src/main.py
+++ env/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -9,6 +9,12 @@ app = FastAPI()
 my_posts = [{"title":"title of post 1", "content":"title of post 1", "id":1},
             {"title":"Favourite Places", "content":"Istanbul", "id":2}]
 
+
+
+def find_id(id):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
 
 
 class Post(BaseModel):
@@ -23,7 +29,7 @@ async def root():
     return {"message":"Welcome to my API"}
 
 @app.get("/posts")
-async def get_post():
+async def get_posts():
     return {"data":my_posts}
 
 @app.post("/posts")
@@ -33,6 +39,21 @@ async def create_posts(post: Post):
     #my_posts.append(post_dict)
     return {"data":my_posts}
 
+
+@app.get("/posts/latest")
+def get_latest_post():
+    return {"post detail": my_posts[len(my_posts) - 1]}
+
+
+@app.get("/posts/{id}")
+def get_post(id: int, response: Response):
+    post = find_id(id=id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {id} not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"status Code":response.status_code}
+    print(post)
+    return {"post detail": post}
 
 #* NOTES:
 
